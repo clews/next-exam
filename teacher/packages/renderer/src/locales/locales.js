dfc406902e75219b39971870b8134401c174349d
clews <clemens.hupka@gmail.com>
2026-01-13 10:54:15 +0100

    Added french translations

--- teacher/packages/renderer/src/locales/locales.js
+++ teacher/packages/renderer/src/locales/locales.js
@@ -1,19 +1,20 @@
-
-import { createI18n } from 'vue-i18n'
+import {createI18n} from 'vue-i18n'
 //import { createI18n } from 'vue-i18n'
 
 import en from './en.json'
 import de from './de.json'
+import fr from './fr.json'
 
 const i18n = createI18n({
     locale: 'de',
     fallbackLocale: 'en',
     legacy: false,
     messages: {
-      en,
-      de
-      }
-  })
+        en,
+        de,
+        fr,
+    }
+})
 
 export default i18n
 
